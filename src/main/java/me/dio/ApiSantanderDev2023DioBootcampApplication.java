94a6c74e176bbdc6c98611047adce3cf46c8aa7a
jsantosdev <jp.santos1454@gmail.com>
2023-10-18 15:06:12 -0300

    Committing these files for god knows why

---
+++ src/main/java/me/dio/ApiSantanderDev2023DioBootcampApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSantanderDev2023DioBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSantanderDev2023DioBootcampApplication.class, args);
+	}
+
+}
