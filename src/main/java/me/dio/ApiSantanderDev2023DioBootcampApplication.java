aee1f187b2333beb8de030dac919c73fee5fda8c
jsantosdev <jp.santos1454@gmail.com>
2023-10-18 17:47:04 -0300

    Implementação das camadas de domínio

--- src/main/java/me/dio/ApiSantanderDev2023DioBootcampApplication.java
+++ src/main/java/me/dio/ApiSantanderDev2023DioBootcampApplication.java
@@ -2,12 +2,27 @@ package me.dio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class ApiSantanderDev2023DioBootcampApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiSantanderDev2023DioBootcampApplication.class, args);
-	}
+	SpringApplication app = new SpringApplication(ApiSantanderDev2023DioBootcampApplication.class);
+        app.setDefaultProperties(Collections
+				.singletonMap("server.port", "8083"));
+        app.run(args);}
+	@Component
+	public class ServerPortCustomizer
+			implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
+		@Override
+		public void customize(ConfigurableWebServerFactory factory) {
+			factory.setPort(8086);
+		}
+	}
 }
